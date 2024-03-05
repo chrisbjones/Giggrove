8a7a90eb299e13fcd19e606d71c2b1410c34de08
Kingsley Osagie <osagiekingsley20@icloud.com>
2024-03-05 18:06:27 +0200

    form error fix

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -12,7 +12,18 @@ class GigsController < ApplicationController
   end
 
   def create
-    
+    @gig = Gig.new(gig_params)
+    if @gig.save
+      redirect_to root_path, notice: 'Gig was successfully created.'
+    else
+      render :new
+    end
   end
+
+  private
+
+    def gig_params
+      params.require(:gig).permit(:name, :category, :description)
+    end
 
 end
