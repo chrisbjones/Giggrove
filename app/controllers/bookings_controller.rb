d6210da7de506c102057ec70d5487fc1945d633f
Pabalelo Sethiba <pabalelosethiba.ps@gmail.com>
2024-03-06 13:37:12 +0200

    updates on bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,22 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
   def new
+    @gig = Gig.find(params[:gig_id])
     @booking = Booking.new
+    # @user = current_user
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user_id = current_user
     if @booking.save
-      redirect_to new_gig_booking(@gig)
+      redirect_to @booking, notice: "Booking was successfullty made"
+    else
+      render :new
   end
 end
 
