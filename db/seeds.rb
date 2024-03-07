5e2f8e28939ce5849b15b8dd287a939d8a960e69
Pabalelo Sethiba <pabalelosethiba.ps@gmail.com>
2024-03-07 11:57:16 +0200

    fixs the cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-require 'faker'
+# # This file should ensure the existence of records required to run the application in every environment (production,
+# # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Example:
+# #
+# #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+# #     MovieGenre.find_or_create_by!(name: genre_name)
+# #   end
 
-puts "Clearing database"
+
+
+# require 'faker'
+
+# puts "Clearing database"
 Gig.destroy_all
 User.destroy_all
 
@@ -23,15 +26,15 @@ users << User.create!(first_name: "Chris", last_name: "J.", email: "chris@giggro
 puts "#{User.count} users created"
 
 
-# puts "Creating gigs"
-# 15.times do
-#   gig = Gig.new(
-#     name: Faker::Company.name,
-#     location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-#     category: "#{Faker::Job.title.downcase}",
-#     description: Faker::Lorem.paragraph.truncate(50),
-#     user_id: users.sample.id
-# #   )
-# #   gig.save!
-# end
-# puts "#{Gig.count} gigs created"
+puts "Creating gigs"
+  15.times do
+   gig = Gig.new(
+     name: Faker::Company.name,
+     location: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+     category: "#{Faker::Job.title.downcase}",
+     description: Faker::Lorem.paragraph.truncate(50),
+     user_id: users.sample.id
+   )
+   gig.save!
+ end
+ puts "#{Gig.count} gigs created"
