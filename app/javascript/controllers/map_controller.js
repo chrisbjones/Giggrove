7ae2102fde55077fb461b6471683ca84e54679ab
Monte Nomkusane <iammontekylan@gmail.com>
2024-03-07 18:00:37 +0200

    mapox and search feature added, must fix mapbox bug

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,11 +8,12 @@ export default class extends Controller {
   }
 
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    console.log('hi from map controller')
+    // mapboxgl.accessToken = this.apiKeyValue
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
-    })
+    // this.map = new mapboxgl.Map({
+    //   container: this.element,
+    //   style: "mapbox://styles/mapbox/streets-v10"
+    // })
   }
 }
